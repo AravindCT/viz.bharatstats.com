eaecee974e3d340cd9aff6f6f44ba6159a805a44
AravindCT <aravind366@gmail.com>
2025-02-24 19:28:20 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,8 @@ import { Navbar, Nav } from 'react-bootstrap'
 export default function Header({ menuItems }) {
   return (
     <Navbar bg="white" expand="lg" sticky="top" className={styles.navbar}>
-      <Navbar.Brand href="/"><b>RAW</b><span className="text-primary">Graphs</span> 2.0</Navbar.Brand>
+    
+      <Navbar.Brand href="/"><b>Bharat</b><span className="text-primary">Stats</span> Viz</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
@@ -18,22 +19,14 @@ export default function Header({ menuItems }) {
           })}
           <a
             role="button"
-            href="https://github.com/rawgraphs/rawgraphs-app/issues"
+           // href="https://github.com/rawgraphs/rawgraphs-app/issues"
             target="_blank"
             rel="noopener noreferrer"
             className="btn btn-sm btn-primary ml-2 d-flex flex-column align-items-center justify-content-center"
           >
-            Report issue
+            Ask AI
           </a>
-          <a
-            role="button"
-            href="https://github.com/sponsors/rawgraphs"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-sm btn-primary ml-2 d-flex flex-column align-items-center justify-content-center"
-          >
-            Support us
-          </a>
+         
         </Nav>
       </Navbar.Collapse>
     </Navbar>
